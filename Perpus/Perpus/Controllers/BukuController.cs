2856b624ba34f65e98c88b403adafbe18d257557
aldi afrizal <aldiafrizal@MacBook-Air-aldi.local>
2022-03-05 22:04:28 +0700

    add api project

---
+++ Perpus/Perpus/Controllers/BukuController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Perpus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class BukuController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public BukuController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            string query = @"select id,nama_buku,tahun_terbit from buku";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BukuAppCon");
+            MySqlDataReader myReader;
+            using(MySqlConnection mycon=new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Perpus/Perpus/Models/Buku.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Perpus.Models
+{
+    public class Buku
+    {
+        public int id { get; set; }
+
+        public string nama_buku { get; set; }
+
+        public DateTime tahun_terbit { get; set; }
+    }
+}
